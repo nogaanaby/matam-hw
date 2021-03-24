c57609b1039e6562cf82e2621793e60c9c4fde2c
shahar12 <shahar.amshili@gmail.com>
2021-03-24 11:13:21 +0100

    "qu 1,2"

---
+++ deletePosIntger.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int numOccurs(int digit , long number );
+
+int main(){
+
+
+    return 0
+}
+
+int numOccurs(int digit , long number){
+    int res = 0
+    
+    return res; 
+}
+  
