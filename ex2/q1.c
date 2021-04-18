b02bca9782965c2b0e85fbba2db2aef4a41bd00f
nogaanaby <nogaanaby@gmail.com>
2021-04-18 21:44:11 +0100

    still doesnt work

--- ex2/q1.c
+++ ex2/q1.c
@@ -20,80 +20,37 @@ bool isDigit(char c){
     return false;
 }
 
-// void swap(char *adr1, char *adr2){
-//     char temp=adr1;
-//     //char temp2=*adr2;
-//     //*adr1="d";//error
-//     //*adr1='d';//error
-//     adr1=adr2;//error
-//     adr2=temp;
-// }
-void stringSwap(char** a, char** b){
-    char *temp = *a;
-    *a = *b;
-    *b = temp;
-}
 
-// void ReorderArray(char *arr,int size){
-//     char *currentCell=&(*arr[0]), *letterCell=&(*arr[0]);
-    
-//     //*utilsCell=&(*arr[size-1]);
-//     //char *afterLastCell=&(*arr[size]);
+void ReorderArray(char **arr,int size){
+    char tmp[5];
+    int currentIndex=0, tmpIndex=0;
 
-//     printf(" currentCell %s \n", currentCell); //"k8g$3%e9oo"
-//     printf(" *currentCell %c \n", *currentCell);// k
-//     printf(" &currentCell %x \n", &currentCell);//fffcbe0
-//     printf(" &*currentCell %x \n", &*currentCell);//403056
-
-//     //printf(" utilsCell %s \n", utilsCell);
-//     //printf(" arr %c \n", *arr);
-//    // printf(" &utilsCell %x \n", &utilsCell);
-
-//     for(;&*currentCell<&currentCell;currentCell++){
-
-//         if(isLetter(*currentCell) && currentCell>letterCell) {// if its a letter
-//             swap(currentCell,letterCell); // move it to the begining
-//             letterCell++;
-//         }else if (!isDigit(*currentCell)){
-//             swap(currentCell,utilsCell); //sawp it to the end
-//             utilsCell--;
-//         }
-//     }
-// }
-
-void ReorderArray(char *arr,int size){
-
-    char *currentCell=arr;
-    char *utilsCell=arr+size-1;
-    char *letterCell=arr;
-
-    char copystr[250];
-    strcpy(copystr, arr);
-
-    int currentIndex=0, utilsIdex=size-1,letterIndex=0;
-
-    for(;currentCell<=utilsCell;currentCell++){
-
-        if(isLetter(*currentCell) && currentCell>letterCell) {// if its a letter
-            stringSwap(&currentCell,&letterCell);// move it to the begining
-            *(arr+currentIndex)=*currentCell;
-            *(arr+letterIndex)=*letterCell;
-            letterCell++;letterIndex++;
-        }else if (!isLetter(*currentCell) && !isDigit(*currentCell)){
-            //swap(currentCell,utilsCell); //sawp it to the end
-            utilsCell--;
+    int *ptr1=*arr+1;
+    for(;*arr<=*arr+size;*arr++){
+        if(isLetter(**arr)) {
+            tmp[tmpIndex]=**arr;
+            tmpIndex++;
         }
-        currentIndex++;
     }
-    char *blaa="kjogg9090";
-    arr=blaa;
+    for(;*arr<=*arr+size;*arr++){
+        if(isDigit(**arr)) {
+            tmp[tmpIndex]=**arr;
+            tmpIndex++;
+        }
+    }
+    for(;*arr<=*arr+size;*arr++){
+        if(!isDigit(**arr)&&!isLetter(**arr)) {
+            tmp[tmpIndex]=**arr;
+            tmpIndex++;
+        }
+    }
+    char *ptr=tmp;
+    *arr=ptr;
 }
 
 
 int main(){
-    char *arr = "k8g$3%e9oo";
-    //kgeoo839$%
-    printf("*arr = %s \n", arr );
-    ReorderArray(arr,10);
-    printf("*arr = %s \n", arr );
+    char *arr = "k8#i^";
+    ReorderArray(&arr,5);
+    printf("*arr %s", arr);
 }
