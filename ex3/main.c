632fa383adb9ea2bd2b9d4d5c92ec1ad40b73c41
nogaanaby <nogaanaby@gmail.com>
2021-05-11 20:04:56 +0100

    clients list works fine now

--- ex3/main.c
+++ ex3/main.c
@@ -12,31 +12,29 @@ void clients(){
     }
     print_clients_list(clients_list);
 }
-void cars(){
-    /* test for car */
-    struct Car list_car[N]; 
-    createCarList(list_car);
-    // print_car_list(list_car);
-    for(int i = 0 ; i < 2 ; i++)
-        addNewCar(list_car);
-    //  struct Client *c = addNewClient();
-    //  print_client(c);
-    printf("\n");
-    for(int i =0 ; i < 10 ;i++)
-        printf("-");
-    printf(" print car list ");
-    for(int i =0 ; i < 10 ;i++)
-        printf("-");
-    printf("\n");
-    print_car_list(list_car);
+// void cars(){
+//     /* test for car */
+//     struct Car list_car[N]; 
+//     createCarList(list_car);
+//     // print_car_list(list_car);
+//     for(int i = 0 ; i < 2 ; i++)
+//         addNewCar(list_car);
+//     printf("\n");
+//     for(int i =0 ; i < 10 ;i++)
+//         printf("-");
+//     printf(" print car list ");
+//     for(int i =0 ; i < 10 ;i++)
+//         printf("-");
+//     printf("\n");
+//     print_car_list(list_car);
 
-    // search  result  in array
-    struct Car *res ; 
-    //res = searchBy_engine_capacity(list_car,2000);
-    if(res){
-        print_car(res);
-    }
-}
+//     // search  result  in array
+//     struct Car *res ; 
+//     //res = searchBy_engine_capacity(list_car,2000);
+//     if(res){
+//         print_car(res);
+//     }
+// }
 
 
 int main(){
@@ -47,10 +45,10 @@ int main(){
     // }
     /*start test sup*/
     clients();
-    {
-        struct Supplier sup_list[K];    
-        run_test_supplier(sup_list);
-    }
+    // {
+    //     struct Supplier sup_list[K];    
+    //     run_test_supplier(sup_list);
+    // }
     return 0;
 }
      
