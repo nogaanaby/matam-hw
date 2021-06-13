e8d6690eb73c6e5aef1bf152cf50844dc83590e7
shahar12 <shahar.amshili@gmail.com>
2021-06-13 18:46:12 +0100

    --- Auto-Git Commit ---

---
+++ ex7_part1/Graph.c
@@ -0,0 +1,9 @@
+
+
+int isMainGroup(int **Graph,int graph_N, int Group[], int group_M){
+    
+
+
+
+    
+}
---
+++ ex7_part1/Graph.h
@@ -0,0 +1,6 @@
+#ifndef GRAPH_h
+#define GRAPH_h
+
+int isMainGroup(int **Graph,int graph_N, int Group[], int group_M);
+
+#endif
